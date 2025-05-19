82929f0c8ed6493039f6aac0643e07ed174f120c
André Victor Sampaio Cavalcante <andre899120@gmail.com>
2025-05-18 22:51:34 -0300

    Novos exercicios feitos

---
+++ aprendizado-jogo-da-forca/cpp/aprendizado-jogo-da-forca.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    char palavra[30], letra[1], secreta[30];
+    int i, tam, chances, acertos;
+    bool acerto;
+
+    tam = 0;
+    chances = 5;
+    acertos = 0;
+    acerto = false;
+    i = 0;
+
+    cout << "Fale ao seu amigo para virar para o outro lado e digite uma palavra: ";
+    cin >> palavra;
+    system("cls");
+
+    while(palavra[i] != '\0'){
+        i++;
+        tam++;
+    }
+
+    for(i = 0; i < 30; i++){
+        secreta[i] = '_';
+    }
+
+    while((chances > 0) && (acertos < tam)){
+        cout << "Chances restantes: " << chances << "\n\n";
+        cout << "Palavra secreta: ";
+        for(i = 0; i < tam; i++){
+            cout << secreta[i];
+        }
+        cout << "\n\nDigite uma letra: ";
+        cin >> letra[0];
+        for(i = 0; i < tam; i++){
+            if(palavra[i] == letra[0]){
+                acerto = true;
+                secreta[i] = palavra[i];
+                acertos++;
+            }
+        }
+        if(!acerto){
+            chances--;
+        }
+        acerto = false;
+        system("cls");
+    }
+
+
+    if(acertos == tam){
+        cout << "Parabens, voce ganhou!!\n";
+    }else{
+        cout << "Que pena, voce perdeu!!\n";
+    }
+
+    system("pause");
+	return 0;
+}
